c18cbbd786fa1463d606f9c40941187cc0d33f1a
Ramadevic <109077282+Ramadevic@users.noreply.github.com>
2025-07-03 03:50:28 -0400

    Update snowflake-deploy.sql

--- .github/workflows/snowflake-deploy.sql
+++ .github/workflows/snowflake-deploy.sql
@@ -1,29 +1,9 @@
-name: Deploy to Snowflake
+USER ROLE ACCOUNTADMIN;
+USE WAREHOUSE DEVOPS_WH;
+USE DATABASE DEVOPS_DB;
+USE SCHEMA COMMON;
 
-on:
-  push:
-    branches:
-      - main  
-
-jobs:
-  deploy:
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Checkout repository
-        uses: actions/checkout@v3
-
-      - name: Install Snowflake CLI using GitHub Action
-        uses: snowflakedb/snowflake-cli-action@v1.5
-        with:
-          cli-version: '3.6.0'  # Optional: remove or update for the latest version
-
-      - name: Run snowflake-deploy.sql using Snowflake CLI
-        env:
-          SNOWFLAKE_ACCOUNT: ${{ secrets.SNOWSQL_ACCOUNT }}
-          SNOWFLAKE_USER: ${{ secrets.SNOWSQL_USER }}
-          SNOWFLAKE_PASSWORD: ${{ secrets.SNOWSQL_PWD }}
-        run: |
-          snow sql \
-            -q "USE ROLE ACCOUNTADMIN; USE WAREHOUSE DEVOPS_WH; USE DATABASE DEVOPS_DB; USE SCHEMA COMMON;" \
-            -f .github/workflows/snowflake-deploy.sql
+CREATE OR REPLACE TABLE TEST TABLE (
+  ID INT,
+  NAME STRING
+);
